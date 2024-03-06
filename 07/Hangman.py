c7cc96ba1134c21a90a380b490d1840238e69fa2
Zdeněk Barth <zdebarth@gmail.com>
2024-03-06 11:04:30 +0100

    7 to 8

--- 07/Hangman.py
+++ 07/Hangman.py
@@ -9,18 +9,29 @@ chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess the letter: ").lower()
-word_guess = ["_"]*len(chosen_word)
+letter_guess = ["_"]*len(chosen_word)
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-position = 0
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-        word_guess[position] = letter
-    else:
-        print("Wrong")
-    position += 1
+lives = 6
 
-print(word_guess)
+while "_" in (letter_guess):
+    position = 0
+   
+    guess = input("Guess the letter: ").lower()
+
+    for letter in chosen_word:
+        if letter == guess:
+            letter_guess[position] = letter
+        else:
+            position += 1
+    if guess not in chosen_word:
+        lives -= 1
+    print(f"You have {lives} lives.")
+    if lives == 0:
+        print("You lose")
+        break
+
+    
+    print("".join(letter_guess))
+
