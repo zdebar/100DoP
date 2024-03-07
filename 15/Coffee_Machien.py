a29caa7facb407c6ea917d8d5fd2f3183b498b51
Zdeněk Barth <zdebarth@gmail.com>
2024-03-07 05:25:59 +0100

    15

---
+++ 15/Coffee_Machien.py
@@ -0,0 +1,33 @@
+# What would u like prompt, offers variaty of coffee
+    # Multiple coffee action
+    # Report check
+    # Off action - ends
+
+
+
+def order():
+    # check resources - if enough, print whats not enough
+    # process coin
+    # show coffee pictur
+    pass
+
+def check_resources():
+    # check resourses
+    # return True, print whats missing
+    pass
+
+def process_coin():
+    # insert coins
+    # round money at two decimal coins
+
+def make_coffe():
+    pass    
+
+
+def report():
+    # print how much of everything, including money
+    pass
+
+def end_machine():
+    # ends_machine
+    pass
