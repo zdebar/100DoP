12311aea119c79caddbda75b97d53d1c0d881420
Zdeněk Barth <zdebarth@gmail.com>
2024-03-09 17:44:50 +0100

    26 Nato Alphabet

---
+++ 26/NATO_Alphabet.py
@@ -0,0 +1,18 @@
+import pandas
+
+df = pandas.read_csv("./26/nato_phonetic_alphabet.csv")
+
+# Create { "A" : "Alpha"}
+data = {row["letter"]: row["code"] for index, row in df.iterrows()}
+print(data)
+
+# Creat a list of NATO code from user inputs
+while True:
+    word = input("Entry word to convert to NATO phonetic alphabet(or E for exit): ").upper()
+    
+    if word == "E":
+        break
+    word_list = [data[letter] for letter in word]
+
+    print(word_list)
+
