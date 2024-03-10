0c492cb4150bcb72c9abab25d6ec5b5f31c2761b
Zdeněk Barth <zdebarth@gmail.com>
2024-03-10 07:26:10 +0100

    finished 34

---
+++ 34/data.py
@@ -0,0 +1,20 @@
+import requests
+
+# Connect to API Endpoint URL
+
+parameters = {
+    "amount": 20,
+    "type": "boolean",
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
+
+
+
+
+
+
+
+
