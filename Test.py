f5bb2c41b2a81a14d4392cc2f76bab6b045225d2
Zdeněk Barth <zdebarth@gmail.com>
2024-03-19 11:11:52 +0100

    Data Structure options

--- Test.py
+++ Test.py
@@ -1,20 +1,4 @@
-class Car:
-    def __init__(self, speed):
-        self.__speed = speed  # Speed is encapsulated
+num12 = {1,2,3}
+num1 = {1,2}
 
-    def get_speed(self):
-        return self.__speed  # Accessor method for speed
-
-    def set_speed(self, speed):
-        if speed < 0:
-            raise ValueError("Speed cannot be negative")
-        self.__speed = speed  # Mutator method for speed
-
-car = Car(100)
-print(car.get_speed())  # Output: 100
-
-# Attempt to access speed directly raises an AttributeError
-print(car.__speed)  # This will raise an AttributeError
-
-# Attempt to set negative speed raises a ValueError
-car.set_speed(-50)  # This will raise a ValueError
+print(num1 - num12 == set())
