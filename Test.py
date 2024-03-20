c3fdf67fd8a6ba6b81ac9cbee6556b6be6507f21
Zdeněk Barth <zdebarth@gmail.com>
2024-03-20 07:32:47 +0100

    Continuting Data Structure

--- Test.py
+++ Test.py
@@ -1,4 +1,9 @@
-num12 = {1,2,3}
-num1 = {1,2}
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = (4, 5, 6, 7, 8)
 
-print(num1 - num12 == set())
+
+def common_elements(tuple1, tuple2):
+    return tuple(set(tuple1)&set(tuple2))
+
+
+print(common_elements(tuple1, tuple2))
