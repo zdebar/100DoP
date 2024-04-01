693771fd773cb4f91bb37375bf3c8b3cd07aa2c6
Zdeněk Barth <zdebarth@gmail.com>
2024-04-01 19:12:30 +0200

    Finished Web Scraping

--- 48/selenium_web.py
+++ 48/selenium_web.py
@@ -1,15 +1,25 @@
 from selenium import webdriver
 
+# Import specifické metody
+from selenium.webdriver.common.by import By
+
 # Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+
+# Set up Selenium
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.amazon.com")
 
 
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cents = driver.find_elements(By.CLASS_NAME, value="a")
+
+
 # Closes only active tab
 driver.close()
+
 
 # Closes entire window
 driver.quit(())
