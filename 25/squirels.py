f879ba71da3803920ec267dc53b8ebf63f54f2ad
Zdeněk Barth <zdebarth@gmail.com>
2024-04-27 13:58:25 +0200

    Deleting Old

--- 25/squirels.py
+++
@@ -1,7 +0,0 @@
-import pandas
-
-data = pandas.read_csv("25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-fur_color = data["Primary Fur Color"].value_counts()
-df = fur_color.reset_index().rename(columns={'index': 'Index', 'Primary Fur Color': 'Fur Color', 'count': 'Count'})
-print(type(df))
-print(df)
--- 27/testing.py
+++
@@ -1,9 +0,0 @@
-
-
-while True:
-    a = input("Vlož číslo: ")
-    try:
-        print(a+2)
-    except:
-        print("Není číslo")
-        continue
